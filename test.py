b9371d6b41bd5d832976f601dfb11748c9c33d21
Blake Lawson <blawson@princeton.edu>
2014-07-23 16:12:03 -0400

    Basic functionality for function to delete servers.

--- test.py
+++ test.py
@@ -8,16 +8,14 @@ import sys
 import time
 
 # Spin up a new server
+# TODO: Add pass variables to set image and number of instances, and
+#       include a verbose option to suppress output.
 def new_server():
+	# Connect to AWS
 	print "Making connection"
 	conn = boto.ec2.connect_to_region("us-east-1")
 
-	rs = conn.get_all_security_groups()
-	sg = None
-	for g in rs:
-		if g.name == "launch-wizard-1":
-			sg = g
-
+	# Initialize the new server
 	print "Making a new instance"
 	reservation = conn.run_instances(
 				image_id="ami-6030f908", 
@@ -26,6 +24,7 @@ def new_server():
 				security_groups=["launch-wizard-1"]
 			)
 
+	# Add a tag to the instance
 	print "Adding a tag"
 	for inst in reservation.instances:
 		add_tag(inst)
@@ -52,8 +51,26 @@ def add_tag(instance, tag={ "Parent":"My Python Script!" }):
 
 
 # Terminate a server with given id
-def kill_server(instance):
-	pass
+def kill_server(instance_id):
+	# Connect to AWS
+	conn = boto.ec2.connect_to_region("us-east-1")
+
+
+	# Find given instance
+	instance_found = False
+	reserves = conn.get_all_reservations()
+	for res in reserves:
+		for inst in res.instances:
+			if inst.id == instance_id:
+				instance_found = True
+				break
+
+	# Terminate Instance
+	if instance_found:
+		conn.terminate_instances(instance_ids=[instance_id])
+		print "Instance %s terminated" % instance_id
+	else:
+		print "Instance not found"
 
 # Manage command line input
 def main(argv):
@@ -68,6 +85,7 @@ def main(argv):
 	# # Read Arguments
 	# args = parser.parse_args(argv)
 
+	# Read command-line input
 	if argv:
 		if argv[0] == "add":
 			new_server()
