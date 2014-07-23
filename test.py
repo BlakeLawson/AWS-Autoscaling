749b72c79082c63d1afd71dccc6d453aea906e0c
Blake Lawson <blawson@princeton.edu>
2014-07-23 15:56:56 -0400

    add_server() function basic functionality

---
+++ test.py
@@ -0,0 +1,83 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+' Script meant to test basic interactions with Amazon AWS using boto.
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+import argparse
+import boto
+import boto.ec2
+import sys
+import time
+
+# Spin up a new server
+def new_server():
+	print "Making connection"
+	conn = boto.ec2.connect_to_region("us-east-1")
+
+	rs = conn.get_all_security_groups()
+	sg = None
+	for g in rs:
+		if g.name == "launch-wizard-1":
+			sg = g
+
+	print "Making a new instance"
+	reservation = conn.run_instances(
+				image_id="ami-6030f908", 
+				instance_type="t2.micro",
+				key_name="blake",
+				security_groups=["launch-wizard-1"]
+			)
+
+	print "Adding a tag"
+	for inst in reservation.instances:
+		add_tag(inst)
+
+	print "Set up complete." 
+	for inst in reservation.instances:
+		print "Server id: %s" % inst
+
+# Add a tag to an existing aws instance
+def add_tag(instance, tag={ "Parent":"My Python Script!" }):
+	# Make sure instanc is actually running
+	status = instance.update()
+	while status == "pending":
+		time.sleep(10)
+		status = instance.update()
+
+	if status == "running":
+		instance.add_tags(tag)
+		print "Added tag to %s." % instance
+		return instance
+	else:
+		print "Could not add tag. Status: %s" % status
+		return None
+
+
+# Terminate a server with given id
+def kill_server(instance):
+	pass
+
+# Manage command line input
+def main(argv):
+	# # Set up parser
+	# parser = argparse.ArgumentParser()
+	# group = parser.add_mutually_exclusive_group()
+	# group.add_argument('add', type=str, nargs=1,
+	# 					help='Add a new server')
+	# group.add_argument('del', type=str, nargs=1,
+	# 					help='Delete a server')
+
+	# # Read Arguments
+	# args = parser.parse_args(argv)
+
+	if argv:
+		if argv[0] == "add":
+			new_server()
+		elif argv[0] == "del":
+			if argv[1]:
+				kill_server(argv[1])
+			else:
+				print "Specify server to delete."
+	else:
+		print "Invalid arguments."
+
+if __name__=="__main__":
+	main(sys.argv[1:])
