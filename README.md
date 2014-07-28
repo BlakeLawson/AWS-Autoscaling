60f1c097d914dcbc8de42f959dfe9dc0090903c8
Blake Lawson <blawson@princeton.edu>
2014-07-28 11:14:50 -0400

    Reviewed all code in controller.py and added comments. I think that the file is ready for testing.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Notes
 =====
 Make sure to listen for response from listener every time a socket is used. That means the Listener should be set up to respond every time is recieves a message.
+
+Replace print statements with log statements
--- controller.py
+++ controller.py
@@ -22,18 +22,6 @@ classes and methods
 class Controller:
 	# Dictionary containing lists of aws instance objects for all worker made by
 	# autoscaling ec2 instances (i.e. made by this script). 
-	'''
-	All 'running' instances should be continuously
-	monitored to identify when no longer needed or if additional
-	instances need to be started. All 'starting' instance should be
-	checked periodically to see when the instance finished booting. 
-	At that point, the controller (this) should send the instance a 
-	list of scripts/commands to run and then move the instance into 
-	the 'running' list. 'ending' instances should shut themselves
-	down, but the controller tracks them in case there is something
-	preventing shut down. In which case, the controller overrides and
-	terminates the instance remotely.
-	''' 
 	auto_instances = { 'running':[], 'starting':[], 'ending':[] }
 
 	# List of all primary instances that serve as parents to auto
@@ -44,6 +32,11 @@ class Controller:
 	LISTENER_PASSWORD = 'real'
 
 	# Dictionary of all standard messages to workers
+	# The 'status' and 'kill' message can be used as is, but the 'run'
+	# message should be concatenated with the command you want the
+	# listener to run. 
+	# TODO: Make commands for each possible run command so that the client 
+	#       does not have to do any concatenation later on
 	MESSAGE = {
 		'status' : LISTENER_PASSWORD + ' status',
 		'kill' : LISTENER_PASSWORD + ' end',
@@ -54,7 +47,7 @@ class Controller:
 	# AS THE PORT BEING USED IN Listener CLASS IN listener.py!!!!
 	SOCKET_PORT = 9989
 
-	def __init__(self, verbose=False, ami='ami-38b27a50', instance_type="t2.micro", key_name="blake", security_groups=["launch-wizard-1"]):
+	def __init__(self, verbose=False, ami="ami-38b27a50", instance_type="t2.micro", key_name="blake", security_groups=["launch-wizard-1"]):
 		'''
 		Initialize member variables
 		'''
@@ -85,15 +78,16 @@ class Controller:
 
 		# Add the new instance to the list of known instances
 		for new_inst in reservation.instances:
-			# Adding the parent attribute to instance in order to make a tag
-			# containing parent information later on. IT IS NOT SAFE TO ASSUME
-			# THAT ALL AUTO INSTANCES HAVE THIS ATTRIBUTE, so code accordingly.
+			# Adding the 'parent' attribute to AWS Instance object in order to 
+			# make a tag containing parent information later on. IT IS NOT 
+			# SAFE TO ASSUME THAT ALL AUTO INSTANCES HAVE THIS ATTRIBUTE, so 
+			# code accordingly.
 			new_inst.parent = inst
 			self.auto_instances['starting'].append(new_inst)
 
 	# Connect to a given AWS EC2 instance. Returns a socket object
 	def connect_to_inst(self, inst):
-		address = isnt.public_dns_name
+		address = inst.public_dns_name
 		port = self.SOCKET_PORT
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
@@ -135,6 +129,8 @@ class Controller:
 	def remove_worker(self, parent_inst):
 		children = self.get_children(parent_inst)
 		
+		# Select the first child in the list of children for the given parent
+		# TODO: Use a more rigorous technique for selecting the worker to kill
 		for child in children:
 			if child in self.auto_instances['running'].values():
 				worker = child
@@ -143,17 +139,17 @@ class Controller:
 		if worker:
 			# Connect to worker
 			conn = self.connect_to_inst(worker)
-			conn.sendall(MESSAGE['kill'])
+			conn.sendall(self.MESSAGE['kill'])
 			# TODO: Add line here to confirm that worker got message
 			conn.close()
-
-			# Record time worker was told to stop
-			# TODO: Find a better way of doing this
-			worker.stop_time = time.time()
 
 			# Update lists
 			self.auto_instances['running'].remove(worker)
 			self.auto_instances['ending'].append(worker)
+
+			# Record the time at which the worker was told to start shutting down. It should be
+			# noted that worker is an AWS Instance object, and I'm adding a new attribute to it.
+			worker.stop_time = time.time()
 		else:
 			# There must not be any running children to this parent_inst
 			return None
@@ -161,6 +157,8 @@ class Controller:
 	# Run scripts and perform other tasks with an auto instance that has just started running
 	def start_up(self, inst):
 		# Add a tag
+		# It should be noted that 'inst' is an AWS Instance object, and 'parent' is an attribute that
+		# this script should have added to the Instance when it was first created.
 		if inst.parent:
 			tag = { "Type":"Child", "Parent":inst.parent.id }
 		else:
@@ -210,9 +208,6 @@ class Controller:
 						self.auto_instances['running'].append(inst)
 					# Case: Ending auto instance
 					elif not state == "terminated" and inst not in self.auto_instances['ending']:
-						# Set stop time to now because it's impossible to know when the instance
-						# was told to terminate
-						inst.stop_time = time.time()
 						self.auto_instances['ending'].append(inst)
 					# Case: Terminated auto_instance
 					else:
@@ -223,7 +218,20 @@ class Controller:
 					pass
 
 # This is where everything happens. It basically calls methods in the Controller class
+'''
+All 'running' instances should be continuously
+monitored to identify when no longer needed or if additional
+instances need to be started. All 'starting' instance should be
+checked periodically to see when the instance finished booting. 
+At that point, the controller (this) should send the instance a 
+list of scripts/commands to run and then move the instance into 
+the 'running' list. 'ending' instances should shut themselves
+down, but the controller tracks them in case there is something
+preventing shut down. In which case, the controller overrides and
+terminates the instance remotely.
+''' 
 def monitor(controller):
+	# Get all existing AWS instances
 	controller.update()
 
 	# Main loop
@@ -242,12 +250,13 @@ def monitor(controller):
 				### Call function to start running tasks/scripts ###
 				####################################################
 				controller.start_up(inst)
-			elif status != 'pending':
-				# If this happens, might have lost connection to the instance
-				print "Strange InstaceState for %s" % inst
-			else:
-				# Status must be 'pending' so keep it in this list
+			elif status == 'pending':
+				# The instance is still starting up
 				pass
+			else:
+				# If this case occurs, I have no idea what's going on. Its
+				# possible that the script couldn't connect to the instance.
+				print "Strange InstanceState for %s" % inst
 
 		# Check on base instances
 		if controller.verbose:
@@ -258,6 +267,8 @@ def monitor(controller):
 			message = controller.MESSAGE["status"]
 			conn.sendall(message)
 		
+			# Get the instance's response
+			# TODO: Add check for 'OK' from Listener
 			data = conn.recv(2048).split()
 			CPU = data[0]
 			disk = data[1]
@@ -269,6 +280,8 @@ def monitor(controller):
 			### Condiditions for new worker here ###
 			########################################
 			'''
+			TODO: Make conditions for creating/removing new workers
+
 			It might be good to make some kind of data structure to keep
 			track of past values for each primary worker. Basically
 			something that either averages past values or something that
@@ -290,8 +303,8 @@ def monitor(controller):
 		At this point, I don't have a good reason to check on the auto instance.
 		Could create worker groups (i.e. a base and its auto instances) and track
 		average status across the group to determine whether more workers are
-		needed. Probably won't be able to do this effectively until after testing
-		using the actual workers.
+		needed. Probably won't be able to do this effectively until testing with
+		the actual workers.
 		'''
 		if controller.verbose:
 			print "monitor(): Checking auto instances"
@@ -301,6 +314,8 @@ def monitor(controller):
 			message = controller.MESSAGE["status"]
 			conn.sendall(message)
 
+			# Get the instance's response
+			# TODO: Add check for 'OK' from Listener
 			data = conn.recv(2048).split()
 			CPU = data[0]
 			disk = data[1]
@@ -309,11 +324,15 @@ def monitor(controller):
 			conn.close()
 
 		# Check on instances that are shutting down
-		# TODO: Implement a way to check how long a worker has been trying to shut down, and
-		# after a certain amount of time, terminate the worker from here.
 		if controller.verbose and controller.auto_instances['ending']:
 			print "monitor(): Checking on instances in the process of shutting down"
 		for inst in controller.auto_instances['ending']:
+			# Add a new attribute to the AWS Instance object inst to record the approximate time 
+			# at which the instance was told to start shutting down. This attribute should have
+			# been added when the instance was first added to the 'ending' list.
+			if not inst.stop_time:
+				inst.stop_time = time.time()
+
 			# Get instance status
 			status = inst.update()
 			if status == "terminated":
@@ -340,6 +359,19 @@ def main(argv):
 	parser.add_argument('-s', '--security', help='group name of the AWS security group to use for the new instances', default='launch-wizard-1')
 	parser.add_argument('-t', '--instance_type', help='The type of AWS instance to use (e.g. "t2.micro")', default='t2.micro')
 	parser.add_argument('-v', '--verbose', help='output what is going on.', action='store_true')
+
+	'''
+	TODO: Rather than using hard coded default options for the parser, it would be 
+	better to make a function that will select the AMI and security groups 
+	from AWS. This could be done by using a standard naming convention for 
+	auto-scaling-related AMI/security groups and then checking which AMI/security
+	groups are the newest or have been labeled as the desired choice for this program 
+	to use.
+
+	For example, name all auto-scaling AMI 'auto_scale_1', 'auto_scale_2', etc. Then, 
+	use boto to get a list of all available AMI and use simple string parsing to 
+	select the highest version.
+	'''
 
 	# Read command-line input
 	args = parser.parse_args(argv)
